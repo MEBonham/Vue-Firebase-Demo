ef9219e2a57aeeb6dfc43d2a3d0354f5c1a38d56
MEBonham <programmer@mckaybonham.com>
2019-05-14 16:39:12 -0600

    Follows tutorial

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,15 +1,28 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+Vue.use(VueFire)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDG3NX64aItkCYr6ZBK4qGvlx_3Ke8MEe8",
+  authDomain: "vue-demo-73a6f.firebaseapp.com",
+  databaseURL: "https://vue-demo-73a6f.firebaseio.com",
+  projectId: "vue-demo-73a6f",
+  storageBucket: "vue-demo-73a6f.appspot.com",
+  messagingSenderId: "843157726219",
+  appId: "1:843157726219:web:47f83cb8355195c3"
+})
+
+export const db = firebase.firestore()
 
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  components: {App},
+  template: '<App/>
 })
--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import firebase from 'firebase'
+
+import Comics from '../components/Comics'
+import Login from '../components/Login'
+import SignUp from '../components/SignUp'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
+    },
+    {
+      path: '/comics',
+      name: 'Comics',
+      component: Comics,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  if (requiresAuth && !currentUser) next('login')
+  else if (!requiresAuth && currentUser) next('comics')
+  else next()
+})
+
+export default router
